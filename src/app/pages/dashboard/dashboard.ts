bd77169a8f8881af30bc4d642ae5b549a38fa5a9
J Geethika <243115243020@msec.edu.in>
2026-06-07 13:19:31 +0530

    Animated DashBoard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,42 +1,30 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
 export class Dashboard {
 
-  constructor(private router: Router) {}
+  sidebarOpen = false;
 
-  goDashboard() {
-    this.router.navigate(['/dashboard']);
-  }
-
-  goTasks() {
-    this.router.navigate(['/tasks']);
-  }
-
-  goPlanner() {
-    this.router.navigate(['/planner']);
-  }
-
-  goAnalytics() {
-    this.router.navigate(['/analytics']);
-  }
-
-  goProfile() {
-    this.router.navigate(['/profile']);
-  }
-
-  createTask() {
-    this.router.navigate(['/tasks']);
-  }
-
-  openAIPlanner() {
-    this.router.navigate(['/planner']);
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
   }
 
 }
