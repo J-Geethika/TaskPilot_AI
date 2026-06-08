2103d971834a9877c7dee3149a9ad69990f1409b
J Geethika <243115243020@msec.edu.in>
2026-06-08 10:14:01 +0530

    PPT ready

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
+
+  sidebarOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  goDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  goTasks() {
+    this.router.navigate(['/tasks']);
+  }
+
+  goPlanner() {
+    this.router.navigate(['/ai-planner']);
+  }
+
+  goTeam() {
+    this.router.navigate(['/team']);
+  }
+
+  goCalendar() {
+    this.router.navigate(['/calendar']);
+  }
+
+  goDocuments() {
+    this.router.navigate(['/documents']);
+  }
 
 }
